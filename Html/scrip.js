893b5da7ec2168414ba80cba609f891688caac76
Panxho-12 <alvarezfran125@gmail.com>
2023-05-23 16:30:25 -0400

    datos
    se muestran los datos por consola

---
+++ Html/scrip.js
@@ -0,0 +1,45 @@
+function datos(){
+    let eNombre= document.getElementById("nombre");
+    let nombre= eNombre.value;
+    console.log(nombre)
+
+    let eMail= document.getElementById("email");
+    let mail= eMail.value;
+    console.log(mail)
+
+    let ePhono= document.getElementById("telefono");
+    let phono= ePhono.value;
+    console.log(phono)
+
+    let ePwd= document.getElementById("pwd")
+    let pdw= ePwd.value;
+    console.log(pdw)
+
+    let eDate= document.getElementById("fecha")
+    let date= eDate.value;
+    console.log(date)
+
+    let eHombre= document.getElementById("hombre")
+    let hombre= eHombre.value;
+    console.log(hombre)
+
+    let eMujer= document.getElementById("mujer")
+    let mujer= eMujer.value;
+    console.log(mujer)
+
+    let eBinario= document.getElementById("nobinario")
+    let binario= eBinario.value;
+    console.log(binario)
+
+    
+
+}
+
+
+
+
+
+
+
+
+document.getElementById("btn").addEventListener("click",datos)
