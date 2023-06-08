f8c7f039a5bce5ba7044b9e79e8ed2c03481b1e3
Panxho-12 <alvarezfran125@gmail.com>
2023-06-07 21:47:13 -0400

    index
    se logra hacer lo visto en clases, solo que la funcion de eliminar hay un error

--- Html/scrip.js
+++ Html/scrip.js
@@ -1,3 +1,118 @@
+var modificar = (listadoNuevo)=>{
+    let eNombre= document.getElementById("nombre");
+    let eMail= document.getElementById("email");
+    let ePhono= document.getElementById("telefono");
+    let ePwd= document.getElementById("pwd")
+    let eDate= document.getElementById("fecha")
+    let eHombre= document.getElementById("hombre")
+    let eMujer= document.getElementById("mujer")
+    let eBinario= document.getElementById("nobinario")
+    let eBtnEditarUp = document.getElementById('btnEditar')
+
+
+    let indice = eBtnEditarUp.value;
+    let nombre= eNombre.value;
+    let mail= eMail.value;
+    let phono= ePhono.value;
+    let pdw= ePwd.value;
+    let date= eDate.value;
+    let hombre= eHombre.checked;
+    let mujer= eMujer.checked;
+    let binario= eBinario.checked;
+
+    listadoNuevo[indice].nombre = nombre;
+    listadoNuevo[indice].mail = mail;
+    listadoNuevo[indice].phono = phono;
+    listadoNuevo[indice].pdw = pdw;
+    listadoNuevo[indice].date = date;
+    listadoNuevo[indice].hombre = hombre;
+    listadoNuevo[indice].mujer = mujer;
+    listadoNuevo[indice].binario = binario;
+    localStorage.setItem('personas',JSON.stringify(listadoNuevo));
+    cargartTabla(lista)
+}
+
+
+var eliminar = (listadoNuevo)=>{
+    let eBtnEliminarUp = document.getElementById('btnEliminar');
+    let indice = eBtnEliminarUp.value;
+    lista = listadoNuevo.filter((p)=>p.id!=indice)
+    localStorage.setItem('personas',JSON.stringify(lista));
+    cargartTabla(lista)
+}
+
+
+var cargarTabla = (listadoNuevo)=>{
+    let eContenedorTabla = document.getElementById("contenedorTabla");
+    let eNombre= document.getElementById("nombre");
+    let eMail= document.getElementById("email");
+    let ePhono= document.getElementById("telefono");
+    let ePwd= document.getElementById("pwd")
+    let eDate= document.getElementById("fecha")
+    let eHombre= document.getElementById("hombre")
+    let eMujer= document.getElementById("mujer")
+    let eBinario= document.getElementById("nobinario")
+
+    render = "<table>"
+    render+= "<tr><th>Nombre</th><th>Email</th><th>Telefono</th><th>Contraseña</th><th>Fecha</th><th>Genero</th><th>Accion</th></tr>"
+    for (let i = 0; i < listadoNuevo.length; i++) {
+        const element = listadoNuevo [i];
+        render+="<tr>"
+        render+="<td>"+element.nombre+"</td>"
+        render+="<td>"+element.email+"</td>"
+        render+="<td>"+element.telefono+"</td>"
+        render+="<td>"+element.password+"</td>"
+        render+="<td>"+element.fecha+"</td>"
+        render+="<td>"+element.genero+"</td>"
+        render+="<td>"
+        render+="<button id='btnEditar"+i+"'>Editar</button>"
+        render+="<button>Eliminar</button>"
+        render+="</td>"
+        render+="</tr>"
+    }
+    render+= "</table>";
+    eContenedorTabla.innerHTML = render;
+    for (let i = 0; i < listadoNuevo.length; i++) {
+        var eBtn = document.getElementById("btnEditar"+i);
+        var eBtn2 = document.getElementById("btnEliminar"+i);
+        let element = listadoNuevo[i]
+        eBtn.addEventListener("click",()=>{
+            eNombre.value = element.nombre;
+            eMail.value = element.mail;
+            ePhono.value = element.phono;
+            ePwd.value = element.pdw;
+            eDate.value = element.date;
+            eHombre.checked = element.hombre
+            eMujer.checked = element.mujer
+            eBinario.checked = element.binario
+            let sEditar = "<button type='button' id='btnEditar' value='"+i+"'>Editar</button>";
+
+            let contenedorBoton = document.getElementById('contenedorBtnExtra');
+            contenedorBoton.innerHTML = sEditar
+            let eBtnEditarUp = document.getElementById('btnEditar');
+            eBtnEditarUp.addEventListener('click',()=>modificar(listadoNuevo))
+        })
+        eBtn2.addEventListener("click",()=>{
+            eNombre.value = element.nombre;
+            eMail.value = element.mail;
+            ePhono.value = element.phono;
+            ePwd.value = element.pdw;
+            eDate.value = element.date;
+            eHombre.checked = element.hombre
+            eMujer.checked = element.mujer
+            eBinario.checked = element.binario
+            let sEditar = "<button type='button' id='btnEliminar' value='"+i+"'>Eliminar</button>";
+
+            let contenedorBoton = document.getElementById('contenedorBtnExtra');
+            contenedorBoton.innerHTML = sEditar
+            let eBtnEditarUp = document.getElementById('btnEliminar');
+            eBtnEditarUp.addEventListener('click',()=>eliminar(listadoNuevo))
+        })     
+    }
+}
+
+
+
 var  datos = ()=> {
     let eNombre= document.getElementById("nombre");
     let nombre= eNombre.value;
@@ -30,15 +145,10 @@ var  datos = ()=> {
     let eBinario= document.getElementById("nobinario")
     let binario= eBinario.checked
     console.log(binario)
-
-
-
-  
-
 }
 
+
 var registro = () =>{
-    let eContenedorTabla = document.getElementById("contenedorTabla")
     let eNombre = document.getElementById("nombre")
     let nombre = eNombre.value;
 
@@ -74,50 +184,20 @@ var registro = () =>{
       }
       else{sexo = "No Binario"}
 
-    // let ingreso = {"nombre":nombre,"email":mail,"telefono":phono,"password":pwd,"fecha":date,"hombre":hombre,"mujer":mujer,"nobinario":binario}
-    let ingreso = {"nombre":nombre,"email":mail,"telefono":phono,"password":pwd,"fecha":date,"genero":sexo}
+
     let listadoIngreso = localStorage.getItem("ingreso")
     let listadoAntiguo = JSON.parse(listadoIngreso);
     if (listadoAntiguo==null){
+        let ingreso = {"id":0,"nombre":nombre,"email":mail,"telefono":phono,"password":pwd,"fecha":date,"genero":sexo}
         listadoNuevo = [ingreso]
     }else{
+        let ingreso = {"id":listadoAntiguo.length,"nombre":nombre,"email":mail,"telefono":phono,"password":pwd,"fecha":date,"genero":sexo}
         listadoNuevo = [...listadoAntiguo,ingreso]
     }
-    console.log(ingreso)
-    console.log(listadoAntiguo)
-    console.log(listadoNuevo)
     localStorage.setItem("ingreso",JSON.stringify(listadoNuevo))
-    
-    //Se comienza con la creacion de la tabla
-    render = "<table>"
-    render+= "<tr><th>Nombre</th><th>Email</th><th>Telefono</th><th>Contraseña</th><th>Fecha</th><th>Genero</th><th>Accion</th></tr>"
-    for (let i = 0; i < listadoNuevo.length; i++) {
-        const element = listadoNuevo [i];
-        render+="<tr>"
-        render+="<td>"+element.nombre+"</td>"
-        render+="<td>"+element.email+"</td>"
-        render+="<td>"+element.telefono+"</td>"
-        render+="<td>"+element.password+"</td>"
-        render+="<td>"+element.fecha+"</td>"
-        render+="<td>"+element.genero+"</td>"
-        render+="<td>"
-        render+="<button id='btnEditar"+i+"'>Editar</button>"
-        render+="<button>Eliminar</button>"
-        render+="</td>"
-        render+="</tr>"
-    }
-    render+= "</table>";
-    eContenedorTabla.innerHTML = render;
-    for (let i = 0; i < listadoNuevo.length; i++) {
-        var eBtn = document.getElementById("btnEditar"+i)
-        let element = listadoNuevo [i];
-        eBtn.addEventListener("click",()=>{alert("hola "+element.nombre+" "+element.genero)})  
-    }
-    //se termina la tabla
+
+    cargarTabla(listadoNuevo)
 }
-
-
-
 
 
 
